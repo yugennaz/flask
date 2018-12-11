01fa7fcba4f4bd159f70271d69068246b84f91a0
Nazira <Baltagaraeva@mail.ru>
2018-12-11 13:24:05 +0600

    Implement list and add methods for cart

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import json
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('hello.html')
+    with open('db.txt', 'r') as f:
+        items = json.load(f)
+        if request.method == 'POST':
+            item = request.form['item']
+            quantity = request.form['quantity']
+            items.update({item:quantity})
+            with open('db.txt', 'w') as f2:
+                json.dump(items, f2)
+        return render_template('hello.html', items=items)
