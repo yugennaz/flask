37de16f13fdce93a5339569a6a6d8111fef2db77
Nazira <Baltagaraeva@mail.ru>
2018-12-13 14:32:30 +0600

    Add method and checkbox Delete

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    with open('db.txt', 'r') as f:
-        items = json.load(f)
+    try:
+        with open('db.txt', 'r') as f:
+            items = json.load(f)
         if request.method == 'POST':
             item = request.form['item']
             quantity = request.form['quantity']
             items.update({item:quantity})
-            with open('db.txt', 'w') as f2:
-                json.dump(items, f2)
-        return render_template('hello.html', items=items)
+            if request.form["delete_button"] == "Delete":
+                del items[item]
+    except KeyError:
+        pass
+    with open('db.txt', 'w') as f2:
+        json.dump(items, f2)
+    return render_template('hello.html', items=items)
