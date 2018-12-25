1152e7e390d633caa52a023d0919bed97fc38a30
Nazira <Baltagaraeva@mail.ru>
2018-12-25 20:02:03 +0600

    Add new add_test

--- app.py
+++ app.py
@@ -3,23 +3,51 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    """
+    Return index page of the web app
+    """
+    return render_template('index.html')
+
+def is_key_item(key):
+    if 'quantity' in key:
+        return False
+    if 'delete' in key:
+        return False
+    if key == 'add':
+        return False
+    return True
 
 
 @app.route('/items', methods=['GET', 'POST'])
 def items():
+    """
+    Returns items page
+    Loads data from db.txt as json
+    """
     with open('db.txt', 'r') as f:
         items = json.load(f)
         if request.method == 'POST':
-            item = request.form['item']
-            quantity = request.form['quantity']
-            items.update({item:quantity})
-            if "delete_button" in request.form.keys() and request.form["delete_button"] == "Delete":
-                del items[item]
-            with open('db.txt', 'w') as f2:
-                json.dump(items, f2)
-    return render_template('items.html', items=items)
+            print(request.method)
+            if 'add' in request.form:
+                items['new'] = 0
+            else:
+                items = {}
+            for key in request.form:
+                print(key)
+                if is_key_item(key):
+                    item = request.form[key]
+                    quantity_key = key + '_quantity'
+                    quantity = request.form[quantity_key]
+                    items[item] = int(quantity)
+            for key in request.form:
+                if key.endswith('delete'):
+                    if request.form[key]:
+                        key = key[:-7]
+                        del items[key]
+            with open('db.txt', 'w') as f:
+                json.dump(items, f)
+        return render_template('items.html', items=items)
--- test_flask.py
+++ test_flask.py
@@ -1,23 +1,87 @@
+import json
 import pytest
-
 from io import StringIO
 from unittest import mock
 from app import app
+
 
 @pytest.fixture
 def client():
     client = app.test_client()
     return client
 
+
+@pytest.fixture
+def db_data():
+    cart = {
+        'banana': 5,
+        'grapes': 100
+    }
+    return StringIO(json.dumps(cart))
+
+
 def test_index(client):
     response = client.get('/')
     response = response.data.decode('utf-8')
-    print(response)
     assert 'Hello' in response
 
-def test_items(client):
+
+def test_post_items_update(client, db_data):
     with mock.patch('app.open') as mocked:
-        mocked.return_value = StringIO('{"test": 1}')
-        response = client.get('/items')    
+        mocked.return_value = db_data
+        response = client.post(
+            '/items',
+            data={
+                'banana': 'banana',
+                'banana_quantity': 5,
+                'grapes': 'apple',
+                'grapes_quantity': 100
+            }
+        )
         response = response.data.decode('utf-8')
-        assert '<li>test:1</li>' in response
+        assert '<input type="text" value="banana" name="banana">' in response
+        assert '<input type="text" value="5" name="banana_quantity">' \
+            in response
+        assert '<input type="text" value="apple" name="apple">' in response
+        assert '<input type="text" value="100" name="apple_quantity">' \
+            in response
+
+
+def test_post_items_remove(client, db_data):
+    with mock.patch('app.open') as mocked:
+        mocked.return_value = db_data
+        response = client.post(
+            '/items', data={
+                'banana': 'banana',
+                'banana_quantity': 5,
+                'grapes': 'grapes',
+                'grapes_quantity': 100,
+                'grapes_delete': 'on'
+            }
+        )
+        response = response.data.decode('utf-8')
+        assert '<input type="text" value="banana" name="banana">' in response
+        assert '<input type="text" value="5" name="banana_quantity">' \
+            in response
+        assert 'grapes' not in response
+
+
+def test_post_items_add(client, db_data):
+    with mock.patch('app.open') as mocked:
+        mocked.return_value = db_data
+        response = client.post(
+            '/items', data={'banana': 'banana',
+                'banana_quantity': 5,
+                'new': 'new',
+                'new_quantity': 0,
+                'add': 'new'
+                }
+            )
+        response = response.data.decode('utf-8')
+        assert '<input type="text" value="banana" name="banana">' in response
+        assert '<input type="text" value="5" name="banana_quantity">' \
+            in response
+        assert '<input type="text" value="new" name="new">' in response
+        assert '<input type="text" value="0" name="new_quantity">' \
+            in response
+        assert'add' in response
